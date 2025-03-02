e5841f9176f42a417b395d7afeb8ca6e68fe2ff2
Polarcito <sebastian.kinderman@gmail.com>
2025-03-02 20:03:58 -0300

    test

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+    $titulo = 'LACRAS';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $titulo; ?></title>
+</head>
+<body>
+    <h1>Hola <?php echo $titulo; ?> (editado)
+</body>
+</html>
